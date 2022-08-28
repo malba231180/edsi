a5c1c67402322405dcc87f1162d7b465d6374655
malba231180 <65570237+malba231180@users.noreply.github.com>
2022-08-27 22:13:58 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 **/
 
 /**
- * Set up the content width value based on the theme's design.02
+ * Set up the content width value based on the theme's design.
  *
  */
 if ( ! isset( $content_width ) )
